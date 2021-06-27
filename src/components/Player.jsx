bacac4b1ee688f7cad6f242fb279e8dff7f28735
Tony Chou <choul@Tonyde-MacBook-Pro.local>
2021-06-28 02:22:55 +0800

    beta-player can play audio from the beginning

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
-import { useAudioPlayer } from "react-use-audio-player"
+import { useAudioPlayer, useAudioPosition } from "react-use-audio-player"
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+
+const formatTime = (seconds) => {
+    const floored = Math.floor(seconds)
+    let from = 14
+    let length = 5
+    // Display hours only if necessary.
+    if (floored >= 3600) {
+        from = 11
+        length = 8
+    }
+
+    return new Date(floored * 1000).toISOString().substr(from, length)
+}
 
 const Player = () => {
 
     const lessons = useSelector(state => state.listen.lessons)
     const [playList, setPlayList] = useState([])
     const [playIndex, setPlayIndex] = useState(0)
+    const [barWidth, setBarWidth] = useState("0%")
+    const seekBarElem = useRef(null)
 
     useEffect(() => {
         let tempPlayList = []
@@ -21,12 +38,45 @@ const Player = () => {
         console.log(tempPlayList[0])
     }, [lessons])
 
-    const { togglePlayPause, ready, loading, playing } = useAudioPlayer({
-        src: `${playList.length > 0 ? playList[0] : ''}`,
+    const { togglePlayPause, play, ready, loading, playing } = useAudioPlayer({
+        src: `${playList.length > 0 ? playList[playIndex] : ''}`,
         format: "mp3",
-        autoplay: false,
-        onend: () => console.log("sound has ended!")
+        autoplay: true,
+        onend: () => {
+            playIndex < playList.length - 1 ? setPlayIndex(playIndex + 1) : setPlayIndex(0)
+            console.log("sound has ended!")
+        }
     })
+
+    const { duration, position, seek, percentComplete } = useAudioPosition({
+        highRefreshRate: true
+    })
+
+    // useEffect(() => {
+    //     player.play()
+    // }, [playIndex])
+
+    useEffect(() => {
+        setBarWidth(`${percentComplete}%`)
+    }, [percentComplete])
+
+    const goTo = useCallback(
+        (event) => {
+            const { pageX: eventOffsetX } = event
+
+            if (seekBarElem.current) {
+                const elementOffsetX = seekBarElem.current.offsetLeft
+                const elementWidth = seekBarElem.current.clientWidth
+                const percent = (eventOffsetX - elementOffsetX) / elementWidth
+                seek(percent * duration)
+            }
+        },
+        [duration, playing, seek]
+    )
+
+    if (duration === Infinity) return null
+    const elapsed = typeof position === "number" ? position : 0
+
 
     return (
         <React.Fragment>
@@ -40,19 +90,16 @@ const Player = () => {
                                         <div id="audio-block" class="">
                                             <div class="audiojs" classname="audiojs" id="audiojs_wrapper0">
                                                 <audio id="player" preload="" src="https://cdn-listening.hle.com.tw/hhe/音檔/L01 Building a Better Relationship_IdiomsAndPhrases_Idioms And Phrases.mp3"></audio>
-                                                <div class="play-pause">
-                                                    {/* <p class="play"></p>
-                                                    <p class="pause"></p>
-                                                    <p class="loading"></p>
-                                                    <p class="error"></p> */}
-                                                    <button onClick={togglePlayPause}>{playing ? "Pause" : "Play"}</button>
+                                                <div class="play-pause d-flex justify-content-center align-items-center" onClick={togglePlayPause}>
+                                                    <FontAwesomeIcon icon={playing ? faPause : faPlay} />
                                                 </div>
-                                                <div class="scrubber">
-                                                    <div class="progress" style={{ width: 0 }}></div>
-                                                    <div class="loaded" style={{ width: 425 }}></div>
+                                                <div class="scrubber" ref={seekBarElem} onClick={goTo}>
+                                                    {/* <div class="progress" style={{ width: 0 }}></div>
+                                                    <div class="loaded" style={{ width: 425 }}></div> */}
+                                                    <div style={{ width: barWidth }} className="progress" />
                                                 </div>
                                                 <div class="time">
-                                                    <em class="played">00:00</em>/<strong class="duration">00:58</strong>
+                                                    <em class="played">{formatTime(elapsed)}</em>/<strong class="duration">{formatTime(duration)}</strong>
                                                 </div>
                                                 <div class="error-message">
                                                 </div>
